1db69b8f7028253ddb388110999fdee2f909d593
Ilan Fonseca <ilan-_@hotmail.com>
2024-10-27 14:05:25 -0300

    fix: resolvido problemas de merge

--- js/LoadProducts.js
+++ js/LoadProducts.js
@@ -6,26 +6,42 @@ $(document).ready(() => {
   const searchResult = $('#search-result');
 
   function renderProductCard(produto) {
+    const ratingStars = generateStars(produto.estrelas);
     return `
       <div class="card bg-gray-100 py-2 px-3 mx-2 my-2 rounded shadow-md">
-        <div class="flex flex-col">
-          <div class="cursor-pointer">
+          <div class="flex flex-col">
             <a href="#produto/${produto.id}">
-              <img class="w-full h-52" src="${produto.imagem}" />
-              <h1 class="text-lg font-medium mb-4">${produto.nome}</h1>
-              <p class="text-2xl font-bold">R$${produto.preco}</p>
-              <p>${produto.quantidade} restantes</p>
+            <div class="cursor-pointer">
+              <div class="flex justify-center w-full mb-4">
+                <img
+                  class="w-64 h-64" 
+                  src="${produto.imagem}"
+                />
+              </div>
+              <div class="flex items-center mb-2 text-gray-800">
+                <div class="w-1/2">
+                  <h1 class="text-lg font-bold">${produto.nome}</h1>
+                </div>
+                <div class="w-1/2 text-right">
+                  <p class="text-2xl font-bold text-blue-600">R$${produto.preco}</p>
+                </div>
+              </div>
+              
+              <div id="products-stars" class="flex mb-2">${ratingStars}</div>
+              <p class="text-gray-500">${produto.quantidade} restantes</p>
+              
+            <div>
             </a>
+
+            <button 
+              class="add-to-cart flex justify-center items-center w-full bg-blue-600 hover:bg-blue-700 transition-colors text-white rounded font-semibold mt-5 uppercase py-1" 
+              data-id="${produto.id}"
+            >
+              <img width="25" height="25" src="/images/icons/cart.svg" />
+              <h1 class="text-sm mx-1 font-bold mr-1">Adicionar ao carrinho</h1>
+            </button>
           </div>
-          <button 
-            class="add-to-cart flex justify-center items-center w-full bg-blue-600 hover:bg-blue-700 transition-colors text-white rounded font-semibold mt-5 uppercase py-1" 
-            data-id="${produto.id}"
-          >
-            <img width="25" height="25" src="/images/icons/cart.svg" />
-            <h1 class="text-sm mx-1 font-bold mr-1">Adicionar ao carrinho</h1>
-          </button>
         </div>
-      </div>
     `;
   }
 
@@ -69,10 +85,8 @@ $(document).ready(() => {
     alert('Item adicionado ao carrinho!');
     $('#cart-items-counter').html(getCartItemsAmount());
   });
-  
 });
 
-// Função para gerar o HTML das estrelas de avaliação
 function generateStars(rating) {
   const maxStars = 5;
   let starsHtml = '';
@@ -84,6 +98,5 @@ function generateStars(rating) {
       starsHtml += '<img src="/images/icons/star-empty.svg" width="20" height="20" alt="Estrela vazia" />';
     }
   }
-
   return starsHtml;
 }
