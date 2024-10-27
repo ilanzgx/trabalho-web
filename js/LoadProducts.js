e738d239fb156d84762949544cb95aeab7dcadcf
Ilan Fonseca <ilan-_@hotmail.com>
2024-10-27 11:35:39 -0300

    feature & refactor: adaptação para mobile e refeita card de produtos

--- js/LoadProducts.js
+++ js/LoadProducts.js
@@ -11,31 +11,42 @@ $(document).ready(() => {
       $('.loading-container').show();
 
       produtos.forEach((produto) => {
+        const ratingStars = generateStars(produto.estrelas); // Gera o HTML das estrelas
         const card = `
-          <div class="card bg-gray-100 py-2 px-3 mx-2 my-2 rounded shadow-md">
-            <div class="flex flex-col">
-              <div class="cursor-pointer">
-                <a href="#produto/${produto.id}">
-                  <img
-                    class="w-full h-52" 
-                    src="${produto.imagem}"
-                  />
-                
-                  <h1 class="text-lg font-medium mb-4">${produto.nome}</h1>
-                  <p class="text-2xl font-bold">R$${produto.preco}</p>
-                  <p>${produto.quantidade} restantes</p>
-                </a>
-              <div>
+        <div class="card bg-gray-100 py-2 px-3 mx-2 my-2 rounded shadow-md">
+          <div class="flex flex-col">
+            <a href="#produto/${produto.id}">
+            <div class="cursor-pointer">
+              <div class="flex justify-center w-full mb-4">
+                <img
+                  class="w-64 h-64" 
+                  src="${produto.imagem}"
+                />
+              </div>
+              <div class="flex items-center mb-2 text-gray-800">
+                <div class="w-1/2">
+                  <h1 class="text-lg font-bold">${produto.nome}</h1>
+                </div>
+                <div class="w-1/2 text-right">
+                  <p class="text-2xl font-bold text-blue-600">R$${produto.preco}</p>
+                </div>
+              </div>
+              
+              <div id="products-stars" class="flex mb-2">${ratingStars}</div>
+              <p class="text-gray-500">${produto.quantidade} restantes</p>
+              
+            <div>
+            </a>
 
-              <button 
-                class="add-to-cart flex justify-center items-center w-full bg-blue-600 hover:bg-blue-700 transition-colors text-white rounded font-semibold mt-5 uppercase py-1" 
-                data-id="${produto.id}"
-              >
-                <img width="25" height="25" src="/images/icons/cart.svg" />
-                <h1 class="text-sm mx-1 font-bold mr-1">Adicionar ao carrinho</h1>
-              </button>
-            </div>
+            <button 
+              class="add-to-cart flex justify-center items-center w-full bg-blue-600 hover:bg-blue-700 transition-colors text-white rounded font-semibold mt-5 uppercase py-1" 
+              data-id="${produto.id}"
+            >
+              <img width="25" height="25" src="/images/icons/cart.svg" />
+              <h1 class="text-sm mx-1 font-bold mr-1">Adicionar ao carrinho</h1>
+            </button>
           </div>
+        </div>
         `;
         container.append(card);
       });
@@ -55,3 +66,19 @@ $(document).ready(() => {
       console.log(`Erro ao carregar produtos: ${error}`);
     });
 });
+
+// Função para gerar o HTML das estrelas de avaliação
+function generateStars(rating) {
+  const maxStars = 5;
+  let starsHtml = '';
+
+  for (let i = 0; i < maxStars; i++) {
+    if (i < rating) {
+      starsHtml += '<img src="/images/icons/star-filled.svg" width="20" height="20" alt="Estrela preenchida" />';
+    } else {
+      starsHtml += '<img src="/images/icons/star-empty.svg" width="20" height="20" alt="Estrela vazia" />';
+    }
+  }
+
+  return starsHtml;
+}
--- js/components/Header.js
+++ js/components/Header.js
@@ -3,16 +3,16 @@ class SpecialHeader extends HTMLElement {
     this.innerHTML = `
       <header class="text-gray-200">
         <div class="flex bg-blue-700 text-center py-2">
-          <div class="flex w-3/12 items-center justify-end px-10">
+          <div class="flex w-2/3 sm:w-3/12 items-center justify-start sm:justify-end px-10">
             <a href="/#inicio">
-              <h1 class="text-3xl">Loja virtual</h1>
+              <h1 class="text-xl sm:text-3xl">Loja virtual</h1>
             </a>
           </div>
-          <div class="relative flex w-5/12 items-center justify-end px-2">
+          <div class="relative hidden sm:flex sm:w-5/12 items-center justify-end px-2">
             <input placeholder="Pesquise o seu produto" class="w-full rounded-lg border border-gray-400 py-3 pl-3 pr-16 text-gray-900" />
             <img src="/images/icons/search.svg" class="absolute mr-2 w-10 cursor-pointer" width="30" height="30" />
           </div>
-          <div class="flex w-2/12 items-center justify-center">
+          <div class="hidden sm:flex sm:w-2/12 items-center justify-center">
             <div class="flex items-center p-1">
               <div class="flex w-1/2 justify-center">
                 <img src="/images/icons/user.svg" width="30" height="30" />
@@ -22,7 +22,7 @@ class SpecialHeader extends HTMLElement {
               </div>
             </div>
           </div>
-          <div class="flex w-2/12 items-center px-6">
+          <div class="flex w-1/3 sm:w-2/12 justify-end sm:justify-start items-center px-6">
             <a href="#carrinho">
               <div class="flex items-center">
                 <img src="/images/icons/cart.svg" />
@@ -32,21 +32,10 @@ class SpecialHeader extends HTMLElement {
           </div>
         </div>
         <div class="bg-blue-800 flex justify-around py-2 px-1">
-          <div>
-            <button class="uppercase font-bold hover:text-gray-50 transition-colors">Promoções</button>
-          </div>
-
-          <div>
-            <button class="uppercase font-bold hover:text-gray-50 transition-colors">Mais vendidos</button>
-          </div>
-
-          <div>
-            <button class="uppercase font-bold hover:text-gray-50 transition-colors">Novidades</button>
-          </div>
-
-          <div>
-            <button class="uppercase font-bold hover:text-gray-50 transition-colors">Preço</button>
-          </div>
+          <button class="uppercase text-xs sm:text-base font-bold hover:text-gray-50 transition-colors">Promoções</button>
+          <button class="uppercase text-xs sm:text-base font-bold hover:text-gray-50 transition-colors">Mais vendidos</button>
+          <button class="uppercase text-xs sm:text-base font-bold hover:text-gray-50 transition-colors">Novidades</button>
+          <button class="uppercase text-xs sm:text-base font-bold hover:text-gray-50 transition-colors">Preço</button>
         </div>
       </header>
     `;
